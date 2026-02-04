178e064c2a5e8f36735c6fc1af9a76c6614ba505
Fabio <fabiopyrodias@gmail.com>
2026-02-04 00:39:10 +0000

    Almost finished ex1. Very short progress. I was sick

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -15,12 +15,41 @@ class DataStream(ABC):
         return data_batch
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
-        pass
+        return {"stream_id": self.__stream_id}
 
 
 class SensorStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+        self.__type = "Environmental Data"
+        self.__readings = 0
+        self.__critical = 0
+
     def process_batch(self, data_batch: List[Any]) -> str:
-        pass
+        readings = 0
+        temp_sum = 0
+        temp_readings = 0
+        for data in data_batch:
+            for item in data.items():
+                readings += 1
+                self.__readings += 1
+                key, value = item
+                if key == "temp":
+                    temp_sum += value
+                    temp_readings += 1
+                    if value >= 30 or value <= 5:
+                        self.__critical += 1
+                elif (key == "humidity" and
+                    (value >= 80 or value <= 20)):
+                        self.__critical += 1
+                elif (key == "pressure" and
+                   (value <= 950 or value >= 1050)):
+                        self.__critical += 1
+        try:
+            temp_average = temp_sum / temp_readings
+        except ZeroDivisionError:
+            return f"{readings} readings processed"
+        return f"{readings} readings processed, avg temp: {temp_average:.1f}ºC"
 
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
@@ -37,7 +66,8 @@ class SensorStream(DataStream):
                                 key == "pressure"):
                                 data.append({key: value})
                         else:
-                            if key == "temp" and value >= 30:
+                            if (key == "temp" and
+                            (value >= 30 or value <= 5)):
                                 data.append({key: value})
                             elif (key == "humidity" and
                             (value >= 80 or value <= 20)):
@@ -48,12 +78,38 @@ class SensorStream(DataStream):
         return data
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
-        pass
+        return {
+            "stream_id": self.__stream_id,
+            "type": self.__type,
+            "readings": self.__readings,
+            "critical": self.__critical
+        }
 
 
 class TransactionStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+        self.__type = "Financial Data"
+        self.__operations = 0
+        self.__large = 0
+
     def process_batch(self, data_batch: List[Any]) -> str:
-        pass
+        operations = 0
+        net_flow = 0
+        for data in data_batch:
+            for item in data.items():
+                operations += 1
+                self.__operations += 1
+                key, value = item
+                if key == "buy":
+                    net_flow += value
+                elif key == "sell":
+                    net_flow -= value
+                if value >= 500:
+                    self.__large += 1
+        if net_flow >= 0:
+            return f"{operations} operations, net flow +{net_flow}"
+        return f"{operations} operations, net flow {net_flow}"
 
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
@@ -76,10 +132,18 @@ class TransactionStream(DataStream):
         return data
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
-        pass
+        return {
+            "stream_id": self.__stream_id,
+            "type": self.__type,
+            "operations": self.__operations,
+            "large": self.__large
+        }
 
 
 class EventStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        pass
+
     def process_batch(self, data_batch: List[Any]) -> str:
         pass
 
